c2ca48d0686f01ab337101b0657d1752289a9181
mans00rahmed <mansoorahmedy2k@gmail.com>
2021-12-06 01:25:38 +0500

    ApplicationDbContext

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -12,5 +12,10 @@ namespace iao.Controllers
         {
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("id :" + id);
+        }
     }
 }
---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iao.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Borrower { get; set; }
+    }
+}
