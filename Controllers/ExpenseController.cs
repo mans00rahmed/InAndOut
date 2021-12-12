3d994f999de1b6843db227656472c70ad53131f1
mans00rahmed <mansoorahmedy2k@gmail.com>
2021-12-13 00:30:44 +0500

    CRUD Operations Done

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -17,21 +17,21 @@ namespace iao.Controllers
             _db = db;
         }
 
+
         public IActionResult Index()
         {
             IEnumerable<Expense> objList = _db.Expenses;
             return View(objList);
+
         }
 
+        // GET-Create
         public IActionResult Create()
         {
             return View();
         }
 
-        //Get Create
-        //Copy Code </>
-
-        // Post Create
+        // POST-Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Expense obj)
@@ -41,7 +41,65 @@ namespace iao.Controllers
                 _db.Expenses.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
 
+        // GET-Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(obj);
         }
